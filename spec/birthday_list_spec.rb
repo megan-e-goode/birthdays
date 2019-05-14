b353a608d99d3aebb531be46ea45aeed5bed6b6f
Megan Goode <megan.e.goode@hotmail.com>
2019-05-14 14:16:38 +0100

    Refectored tests: Removed duplicate tests.

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -14,11 +14,6 @@ describe Birthday do
     expect {bday.add("Me", "1996-04-17")}.to_not raise_error
   end
 
-  it '.add to birthday_list array' do
-    bday.add("Me", "1996-04-17")
-    expect(bday.birthday_list).to include({ name: "Me", dob: "1996-04-17" })
-  end
-
   it '.add multiple birthdays' do
     bday.add("Me", "1996-04-17")
     bday.add("Jen", "1994-08-19")
@@ -29,10 +24,6 @@ describe Birthday do
   end
 
   # Show
-  it 'Birthday responds to .show' do
-    expect(bday).to respond_to(:show)
-  end
-
   it '.show displays list of birthdays' do
     expect(bday.show).to eql(bday.birthday_list)
   end
